5a9d2def0eb757a45451350914e16198cc3d8401
anjanagunasekera007 <anjanagunasekera007@gmail.com>
2017-11-28 10:45:19 +0530

    Add Runnable clients

---
+++ IntegrationTests/src/test/java/org/ballerinalang/integration/tests/Clients/ClientSlowWriting.java
@@ -0,0 +1,66 @@
+package org.ballerinalang.integration.tests.Clients;/*
+* Copyright (c) $today.year, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import org.testng.Assert;
+import org.wso2.carbon.protocol.emulator.dsl.Emulator;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientConfigBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientRequestBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseProcessorContext;
+
+import java.io.File;
+
+public class ClientSlowWriting implements Runnable{
+
+    private String echoBackServerPath = "/services/client/normal";
+    private File plainFile = new File("/home/anjana/work/Ballerina-Integration-Test-Framework/" +
+            "IntegrationTests/src/test/resources/files/100KB.txt");
+
+    private String responseBody = "{\"glossary\":{\"title\":\"exampleglossary\",\"GlossDiv\":{\"title\":\"S\"," +
+            "\"GlossList\":{\"GlossEntry\":{\"ID\":\"SGML\",\"SortAs\":\"SGML\"," +
+            "\"GlossTerm\":\"StandardGeneralizedMarkupLanguage\",\"Acronym\":\"SGML\"," +
+            "\"Abbrev\":\"ISO8879:1986\",\"GlossDef\":{\"para\":\"Ameta-markuplanguage," +
+            "usedtocreatemarkuplanguagessuchasDocBook.\",\"GlossSeeAlso\":[\"GML\"," +
+            "\"XML\"]},\"GlossSee\":\"markup\"}}}}}";
+
+    @Override
+    public void run() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host("127.0.0.1")
+                                .port(Integer.parseInt("9090")).withWritingDelay(3000)
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(echoBackServerPath)
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals(response.getReceivedResponseContext().getResponseBody(), responseBody,
+                "The received response body is not same as the expected");
+        Assert.assertEquals(response.getReceivedResponse().headers().get(HttpHeaders.Names.CONTENT_TYPE),
+                HttpHeaders.Values.APPLICATION_JSON,
+                "The received ContentType header value is different from that expected");
+
+    }
+}
